24158ef0e50e12ec005588cf5c5b4e7eca74f290
lewiis252 <krystianoleniacz@gmail.com>
2023-03-20 18:27:31 +0100

    cw2

--- cw2.R
+++ cw2.R
@@ -3,4 +3,18 @@ rzis <- read.csv("RZiS.csv", row.names = 1)
 rpp <- read.csv("RPP.csv", row.names = 1)
 
 # wskaźnik bieżacej płynności
-bilans$
+bilans$Aktywa.obrotowe / bilans$Zobowiązania.krótkoterminowe
+
+# wskaźnik szybkiej płynności
+(bilans$Aktywa.obrotowe - bilans$Zapasy - bilans$Krótkoterminowe.rozliczenia.międzyokresowe) / bilans$Zobowiązania.krótkoterminowe
+
+# wskaźnik płynności gotówkowej
+bilans$Środki.pieniężne.oraz.inne.aktywa.finansowe / bilans$Zobowiązania.krótkoterminowe
+
+# Wskaźnik wydajności gotówkowej sprzedaży
+
+# Wskaźnik wydajności gotówkowej zysku
+
+# Wskaźnik wydajności gotówkowej majątku
+
+#  Wskaźnik ogólnej wystarczalności gotówki operacyjnej
