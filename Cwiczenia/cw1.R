b2104be684f831dd13f7643fb245b3555944e425
lewiis252 <krystianoleniacz@gmail.com>
2023-03-20 18:08:51 +0100

    changes

--- Cwiczenia/cw1.R
+++
@@ -1,3 +0,0 @@
-bilans <- read.csv('Bilans.csv', row.names=1)
-rzis <- read.csv('RZiS.csv', row.names=1)
-rpp <- read.csv('RPP.csv', row.names=1)
---
+++ cw2.R
@@ -0,0 +1,6 @@
+bilans <- read.csv("Bilans.csv", row.names = 1)
+rzis <- read.csv("RZiS.csv", row.names = 1)
+rpp <- read.csv("RPP.csv", row.names = 1)
+
+# wskaźnik bieżacej płynności
+bilans$
